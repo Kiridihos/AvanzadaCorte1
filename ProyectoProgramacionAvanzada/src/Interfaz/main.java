0b2414e648e21297708372cd61f9ad100cb72840
kiridihos <47702142+Kiridihos@users.noreply.github.com>
2021-08-30 22:07:23 -0500

    conexion exitosa BD
    This is the correct conexion with postgres BD

--- ProyectoProgramacionAvanzada/src/Interfaz/main.java
+++ ProyectoProgramacionAvanzada/src/Interfaz/main.java
@@ -1,10 +1,28 @@
 package Interfaz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 import javax.swing.JFrame;
 
 public class main {
 	public static void main(String[] args) {
 		JFrame main=new  principalJF() ;
 		main.setVisible(true);
+		
+		Connection connection=null;
+		try {
+			Class.forName("org.postgresql.Driver");
+			connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/projectC1","postgres","12345");
+			
+			if(connection!=null) {
+				System.out.println("Conexion exitosa!");
+			}else {
+				System.out.println("Conexion Erronea");
+			}
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+		
 	}
 }
